7fdd16d1bcc928f870d97696bfaf59b146e6a7e3
Randy Valis <randy.valis@gmail.com>
2021-05-29 18:34:10 -0400

    Updated drone.yml

--- tests/conanfile.py
+++ tests/conanfile.py
@@ -1,18 +1,9 @@
-from conans import ConanFile, AutoToolsBuildEnvironment
-from conans import tools
-import os
+from atsconan import ATSConan
 
-class ATSConan(ConanFile):
+class ATSConan(ATSConan):
     name = "ats-unit-testing-tests"
     version = "0.1"
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "atsmake"
-    exports_sources = "*"
     requires = "ats-unit-testing/0.1@randy.valis/testing"
-
-    def build(self):
-        atools = AutoToolsBuildEnvironment(self)
-        atools.make()
 
     def package(self):
         self.copy("target/tests", dst="target", keep_path=False)
